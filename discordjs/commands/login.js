50f931ff5afd7e46e121a83bfcbfac9ab335be97
Darklit <0lies18pies@gmail.com>
2023-11-05 13:35:08 -0500

    Commit login command for convenience with using backup login method

---
+++ discordjs/commands/login.js
@@ -0,0 +1,33 @@
+const { SlashCommandBuilder, StringSelectMenuBuilder, StringSelectMenuOptionBuilder, ActionRowBuilder } = require('discord.js');
+const config = require('../config.js');
+const { MongoClient, RegExp } = require('mongodb');
+const fs = require('node:fs');
+const crypto = require('crypto');
+
+const mongoClient = new MongoClient(config.mongo_uri);
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('register')
+		.setDescription('Register your account for OpenATBP!'),
+	async execute(interaction) {
+		interaction.guild.roles.fetch().then((roles) => {
+			var hasAccess = false;
+			for(var r of roles){
+				if(r[1].members != undefined){
+					console.log(`Boolean 1: ${config.register_roles.includes(r[0])} | Boolean 2: ${r[1].members.has(interaction.member.id)}`);
+					if(config.register_roles.includes(r[0]) && r[1].members.has(interaction.member.id)){
+						hasAccess = true;
+						break;
+					}
+				}
+			}
+			if(hasAccess){
+				interaction.reply({content: `Login at ${config.oauth_url} and the copy & paste the link provided into Waterfox / Pale Moon. Sorry for the inconvenience!`, ephemeral: true});
+			}else{
+				interaction.reply({content: 'You are not authorized to register at this time!', ephemeral: true});
+			}
+		}).catch(console.error);
+
+	},
+};
